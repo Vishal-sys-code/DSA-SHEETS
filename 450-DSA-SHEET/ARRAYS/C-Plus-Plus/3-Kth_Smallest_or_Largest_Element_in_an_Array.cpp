c282c1742597285209637e15dd58f0e8fe221e11
Vishal Pandey <bishalpandey2001@gmail.com>
2022-09-22 23:51:00 +0530

    added '3rd array'

--- 450-DSA-SHEET/ARRAYS/C-Plus-Plus/3-Kth_Smallest_or_Largest_Element_in_an_Array.cpp
+++ 450-DSA-SHEET/ARRAYS/C-Plus-Plus/3-Kth_Smallest_or_Largest_Element_in_an_Array.cpp
@@ -13,6 +13,14 @@ int main(){
     int k;
     cout << "Enter the Kth value to find the Smallest/Largest Value in an array: " << endl;
     cin>>k;
-    
+    // sorting the array in the increasing order
+    sort(arr,arr+n);
+    cout << "The sorted array in the increasing order is: " << endl;
+    for(int i=0; i<n;i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    int ans = arr[k-1];
+    cout << "The kth Smallest/Largest Element in the array is: " << ans << endl;
     return 0;
 }
